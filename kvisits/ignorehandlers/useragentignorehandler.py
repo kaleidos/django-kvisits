e4fc12c447b8c286d87acc17a9df08cade28a5c1
Jesús Espino <jesus.espino@kaleidos.net>
2012-11-16 16:29:09 +0100

    More robust kvisits user agent ignore handler

--- kvisits/ignorehandlers/useragentignorehandler.py
+++ kvisits/ignorehandlers/useragentignorehandler.py
@@ -5,6 +5,6 @@ from kvisits.settings import KVISITS_IGNORE_USER_AGENTS
 class UserAgentIgnoreHandler(IgnoreHandlerBase):
     def check(self, request, **kwargs):
         for regex in KVISITS_IGNORE_USER_AGENTS:
-            if re.match(regex, request.META['HTTP_USER_AGENT']):
+            if re.match(regex, request.META.get('HTTP_USER_AGENT', '')):
                 return True
         return False
